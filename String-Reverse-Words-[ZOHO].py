875e5805b5e451c888e9c1790cffb36fce2b9870
Kishore <58047720+kishorercse@users.noreply.github.com>
2022-01-04 08:42:58 +0530

    Create String-Reverse-Words-[ZOHO].py

---
+++ String-Reverse-Words-[ZOHO].py
@@ -0,0 +1,30 @@
+"""
+A string S is passed as the input. The program must reverse the order of the words in the string and print them as the output.
+
+Input Format:
+The first line will contain S.
+
+Output Format:
+The first line will contain the words in the string S in the reverse order.
+
+Boundary Conditions:
+Length of S is from 5 to 100.
+
+Example Input/Output 1:
+Input:
+Today is Friday
+
+Output:
+Friday is Today
+
+Example Input/Output 2:
+Input:
+five six ten eleven
+
+Output:
+eleven ten six five
+"""
+l=input().split()
+n=len(l)
+for i in range(n-1,-1,-1):
+    print(l[i],end=' ')
